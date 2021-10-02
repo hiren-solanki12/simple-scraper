b90dc3e459a37bbce222318a98e52522ca9fad98
hiren-solanki12 <72147532+hiren-solanki12@users.noreply.github.com>
2021-10-02 12:43:50 +0530

    create-google scraper

---
+++ scraper.py
@@ -0,0 +1,60 @@
+import os
+import selenium
+from selenium import webdriver
+import time
+from PIL import Image
+import io
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+#Install Driver
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+search_url = "https://www.google.com/search?q={q}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568"
+driver.get(search_url.format(q='Car'))
+
+
+img_urls = set()
+for i in  range(0,len(imgResults)):
+    img=imgResults[i]
+    try:
+        img.click()
+        time.sleep(2)
+        actual_images = driver.find_elements_by_css_selector('img.n3VNCb')
+        for actual_image in actual_images:
+            if actual_image.get_attribute('src') and 'https' in actual_image.get_attribute('src'):
+                img_urls.add(actual_image.get_attribute('src'))
+    except ElementClickInterceptedException or ElementNotInteractableException as err:
+        print(err)
+        
+        
+  os.chdir('C:/Hiren Solanki/WebScrapping/Dataset1')
+baseDir=os.getcwd()
+
+for i, url in enumerate(img_urls):
+    file_name = f"{i:150}.jpg"    
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - COULD NOT DOWNLOAD {url} - {e}")
+
+    try:
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert('RGB')
+        
+        file_path = os.path.join(baseDir, file_name)
+        
+        with open(file_path, 'wb') as f:
+            image.save(f, "JPEG", quality=85)
+        print(f"SAVED - {url} - AT: {file_path}")
+    except Exception as e:
+        print(f"ERROR - COULD NOT SAVE {url} - {e}")
+        
+from selenium import webdriver 
+opts = webdriver.ChromeOptions()
+opts.headless =True
+driver =webdriver.Chrome(ChromeDriverManager().install())
